7ffbe82429d3b6f2626e41c9d31dca39ef8518f8
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2022-03-22 21:00:49 +0700

    Add blog post

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -1,8 +1,10 @@
 // * Import from latest posts
+import firstapptomsstore from "./220322.md";
 import introduction from "./220217.md";
 import testing from "./220215.md";
 
 export default {
+  firstapptomsstore,
   introduction,
   testing,
 };
