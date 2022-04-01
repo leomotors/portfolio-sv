202b4dc0e360c474b88959ba01d0df45076e1c37
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2022-04-01 01:59:35 -0700

    Add a Blog

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -1,9 +1,11 @@
 // * Import from latest posts
+import af2022 from "./220401.md";
 import firstapptomsstore from "./220322.md";
 import introduction from "./220217.md";
 import testing from "./220215.md";
 
 export default {
+  af2022,
   firstapptomsstore,
   introduction,
   testing,
